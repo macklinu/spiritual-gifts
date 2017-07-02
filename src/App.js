d01cc56d7e319a10e377ed7dc0f767a4a6eda5c5
Macklin Underdown <macklinu@users.noreply.github.com>
2017-07-02 19:02:52 -0400

    refactor(Explanation): extract Explanation component file

--- src/App.js
+++ src/App.js
@@ -12,37 +12,10 @@ import {
   Text,
 } from 'rebass'
 import ButtonGroup from './components/ButtonGroup'
+import Explanation from './components/Explanation'
 import questions from './questions'
 import gifts from './gifts'
 import calculateGifts from './calculateGifts'
-
-const Explanation = () =>
-  <div>
-    <p>
-      Respond to each question on the Spiritual Gifts Assessment (found on the
-      next few pages) according to the following scale:
-    </p>
-    <ol>
-      <li>Not at All / Never True</li>
-      <li>Sometimes / True Once in a While</li>
-      <li>Most of the Time / Usually True</li>
-      <li>Consistently / Definitely True</li>
-    </ol>
-    <p>
-      <b>
-        Answer according to who you <i>are</i>, not who you would like to be, or
-        think you ought to be.
-      </b>
-    </p>
-    <p>
-      Don’t labor over each question. Just read it and answer it. Click the
-      first answer that pops into your mind.
-    </p>
-    <p>
-      <i>Pray</i> before and during the process of taking the assessment. Let
-      God speak to you about this. After all, they’re His gifts.
-    </p>
-  </div>
 
 const ProgressIndicator = ({ currentQuestion, totalQuestions }) =>
   <Panel theme="info">
---
+++ src/components/Explanation/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default () =>
+  <div>
+    <p>
+      Respond to each question on the Spiritual Gifts Assessment (found on the
+      next few pages) according to the following scale:
+    </p>
+    <ol>
+      <li>Not at All / Never True</li>
+      <li>Sometimes / True Once in a While</li>
+      <li>Most of the Time / Usually True</li>
+      <li>Consistently / Definitely True</li>
+    </ol>
+    <p>
+      <b>
+        Answer according to who you <i>are</i>, not who you would like to be, or
+        think you ought to be.
+      </b>
+    </p>
+    <p>
+      Don’t labor over each question. Just read it and answer it. Click the
+      first answer that pops into your mind.
+    </p>
+    <p>
+      <i>Pray</i> before and during the process of taking the assessment. Let
+      God speak to you about this. After all, they’re His gifts.
+    </p>
+  </div>
