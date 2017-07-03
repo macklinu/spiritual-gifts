6bb94fae374352c72093e10654b65948925563c3
Macklin Underdown <macklinu@users.noreply.github.com>
2017-07-03 10:36:51 -0400

    refactor(App): recompose all the things

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 // @ts-check
 
-import React, { Component } from 'react'
+import React from 'react'
 import { List, Map } from 'immutable'
 import { Button, Container, Space } from 'rebass'
+import compose from 'recompose/compose'
+import withState from 'recompose/withState'
+import withHandlers from 'recompose/withHandlers'
+import setDisplayName from 'recompose/setDisplayName'
 import Explanation from './components/Explanation'
 import ProgressIndicator from './components/ProgressIndicator'
 import Question from './components/Question'
@@ -11,63 +15,59 @@ import questions from './questions'
 import gifts from './gifts'
 import calculateGifts from './calculateGifts'
 
-class App extends Component {
-  state = {
-    data: Map({
+const enhance = compose(
+  setDisplayName('App'),
+  withState(
+    'data',
+    'updateData',
+    Map({
       currentQuestion: undefined,
       totalQuestions: questions.size,
       answers: List(),
-    }),
-  }
+    })
+  ),
+  withHandlers({
+    onBegin: ({ updateData }) => () =>
+      updateData(data => data.set('currentQuestion', 0)),
+    onAnswered: ({ updateData }) => answer =>
+      updateData(data =>
+        data
+          .update('answers', value => value.push(answer))
+          .update('currentQuestion', value => value + 1)
+      ),
+  })
+)
 
-  render() {
-    const data = this.state.data
-    return (
-      <Container>
-        {data.get('currentQuestion') === undefined &&
-          <div>
-            <h1>Spiritual Gifts Assessment</h1>
-            <Explanation />
-            <Button onClick={this.handleBeginClick}>Begin</Button>
-          </div>}
-        {data.get('currentQuestion') < data.get('totalQuestions') &&
-          <div>
-            <Question
-              text={questions.get(data.get('currentQuestion'))}
-              onAnswered={this.handleAnswered}
-            />
-            <Space x={2} />
-            <ProgressIndicator
-              currentQuestion={data.get('currentQuestion')}
-              totalQuestions={data.get('totalQuestions')}
-            />
-          </div>}
-        {data.get('currentQuestion') === data.get('totalQuestions') &&
-          <div>
-            <h1>Your Spiritual Gifts</h1>
-            <ol>
-              {calculateGifts(data.get('answers'))
-                .take(3)
-                .keySeq()
-                .map(key => gifts.get(key))
-                .map(gift => <Gift {...gift} />)}
-            </ol>
-          </div>}
-      </Container>
-    )
-  }
-
-  handleBeginClick = () => {
-    this.setState(({ data }) => ({ data: data.set('currentQuestion', 0) }))
-  }
-
-  handleAnswered = answer => {
-    this.setState(({ data }) => ({
-      data: data
-        .update('answers', value => value.push(answer))
-        .update('currentQuestion', value => value + 1),
-    }))
-  }
-}
-
-export default App
+export default enhance(({ data, onBegin, onAnswered }) =>
+  <Container>
+    {data.get('currentQuestion') === undefined &&
+      <div>
+        <h1>Spiritual Gifts Assessment</h1>
+        <Explanation />
+        <Button onClick={onBegin}>Begin</Button>
+      </div>}
+    {data.get('currentQuestion') < data.get('totalQuestions') &&
+      <div>
+        <Question
+          text={questions.get(data.get('currentQuestion'))}
+          onAnswered={onAnswered}
+        />
+        <Space x={2} />
+        <ProgressIndicator
+          currentQuestion={data.get('currentQuestion')}
+          totalQuestions={data.get('totalQuestions')}
+        />
+      </div>}
+    {data.get('currentQuestion') === data.get('totalQuestions') &&
+      <div>
+        <h1>Your Spiritual Gifts</h1>
+        <ol>
+          {calculateGifts(data.get('answers'))
+            .take(3)
+            .keySeq()
+            .map(key => gifts.get(key))
+            .map(gift => <Gift {...gift} />)}
+        </ol>
+      </div>}
+  </Container>
+)
