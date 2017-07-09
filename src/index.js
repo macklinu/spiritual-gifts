e109e3ee82f917088e90da2cace30898dbb9690d
Macklin Underdown <macklinu@users.noreply.github.com>
2017-07-08 21:37:50 -0400

    feat(theme): add rebass theme

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import { Provider } from 'rebass'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+import theme from './theme'
 
 ReactDOM.render(
-  <Provider>
+  <Provider theme={theme}>
     <App />
   </Provider>,
   document.getElementById('root')
