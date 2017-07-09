f54a381006dd3fb1a1259fbba178c5d40eaa840a
Macklin Underdown <macklinu@users.noreply.github.com>
2017-07-08 22:23:10 -0400

    fix(theme): lessen border radius

--- src/theme.js
+++ src/theme.js
@@ -1,4 +1,5 @@
 export default {
   font:
     '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+  radius: 2,
 }
