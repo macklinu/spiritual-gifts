5a63a505aebd69442df76bf45a642825066ed1a7
Macklin Underdown <macklinu@users.noreply.github.com>
2017-07-09 21:22:02 -0400

    refactor(Progress): set component display name

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import compose from 'recompose/compose'
+import setDisplayName from 'recompose/setDisplayName'
 import mapProps from 'recompose/mapProps'
 import { Text } from 'rebass'
 
-const enhance = mapProps(({ currentQuestion, totalQuestions }) => ({
-  text: `${currentQuestion + 1} of ${totalQuestions}`,
-}))
+const enhance = compose(
+  setDisplayName('Progress'),
+  mapProps(({ currentQuestion, totalQuestions }) => ({
+    text: `${currentQuestion + 1} of ${totalQuestions}`,
+  }))
+)
 
 export default enhance(({ text }) => <Text children={text} />)
