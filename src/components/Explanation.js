161252f535c4d5ccaa65117795b1fec288944eb0
Macklin Underdown <macklinu@users.noreply.github.com>
2017-08-16 23:43:49 -0400

    fix: update intro message

--- src/components/Explanation.js
+++ src/components/Explanation.js
@@ -1,13 +1,23 @@
 import React from 'react'
+import styled from 'styled-components'
 import setDisplayName from 'recompose/setDisplayName'
 import ButtonGroup from './ButtonGroup'
+import { Text } from 'rebass'
+
+const Container = styled.div`
+  > * {
+    margin: 16px 0px 16px 0px;
+  }
+`
 
 export default setDisplayName('Explanation')(() =>
-  <div>
-    <p>
-      Respond to each question on the Spiritual Gifts Assessment according to
-      the following scale:
-    </p>
+  <Container>
+    <Text>
+      This Spiritual Gifts Assessment assumes that all gifts of the Holy Spirit
+      mentioned in 1 Corinthians 12, Romans 12, and Ephesians 4 are still given
+      to the church.
+    </Text>
+    <Text>Respond to each question according to the following scale:</Text>
     <ButtonGroup
       labels={[
         'Not at All / Never True',
@@ -16,19 +26,17 @@ export default setDisplayName('Explanation')(() =>
         'Consistently / Definitely True',
       ]}
     />
-    <p>
-      <b>
-        Answer according to who you <i>are</i>, not who you would like to be, or
-        think you ought to be.
-      </b>
-    </p>
-    <p>
+    <Text bold>
+      Answer according to who you are, not who you would like to be, or think
+      you ought to be.
+    </Text>
+    <Text>
       Don’t labor over each question. Just read it and answer it. Click the
       first answer that pops into your mind.
-    </p>
-    <p>
-      <i>Pray</i> before and during the process of taking the assessment. Let
-      God speak to you about this. After all, they’re His gifts.
-    </p>
-  </div>
+    </Text>
+    <Text>
+      Pray before and during the process of taking the assessment. Let God speak
+      to you about this. After all, they’re His gifts.
+    </Text>
+  </Container>
 )
